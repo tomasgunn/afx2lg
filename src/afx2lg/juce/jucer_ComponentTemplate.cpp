ae6b7ef932da8d37402dd35294475f6d939d6013
tomasgunn@gmail.com <tomasgunn@gmail.com@403f711a-7992-8279-5a03-139aa5e7b863>
2013-02-25 20:46:24 +0000

    Add component template files for the jucer.

---
+++ src/afx2lg/juce/jucer_ComponentTemplate.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) 2013, Tomas Gunnarsson
+// All rights reserved.
+
+// This is a generated file created by the Jucer!
+
+//[Headers] You can add your own extra header files here...
+//[/Headers]
+%%includeFilesCPP%%
+
+//[MiscUserDefs] You can add your own user definitions and misc code here...
+//[/MiscUserDefs]
+
+%%className%%::%%className%%(%%constructorParams%%)
+%%initialisers%%{
+  %%constructor%%
+
+  //[Constructor] You can add your own custom stuff here..
+  //[/Constructor]
+}
+
+%%className%%::~%%className%%() {
+  //[Destructor_pre]. You can add your own custom destruction code here..
+  //[/Destructor_pre]
+
+  %%destructor%%
+
+  //[Destructor]. You can add your own custom destruction code here..
+  //[/Destructor]
+}
+
+%%methodDefinitions%%
+
+//[MiscUserCode] You can add your own definitions of your custom methods or any other code here...
+//[/MiscUserCode]
+
+#if 0
+/*  -- Jucer information section --
+
+    This is where the Jucer puts all of its metadata, so don't change anything in here!
+
+BEGIN_JUCER_METADATA
+
+%%metadata%%
+END_JUCER_METADATA
+*/
+#endif
+
+%%staticMemberDefinitions%%
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
---
+++ src/afx2lg/juce/jucer_ComponentTemplate.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2013, Tomas Gunnarsson
+// All rights reserved.
+
+// This is a generated file created by the Jucer!
+
+#pragma once
+#ifndef %%headerGuard%%
+#define %%headerGuard%%
+
+//[Headers]     -- You can add your own extra header files here --
+#include "JuceHeader.h"
+//[/Headers]
+%%includeFilesH%%
+
+//[Comments]
+// An auto-generated component, created by the Jucer.
+// Describe your class and how it works here!
+//[/Comments]
+%%classDeclaration%% {
+ public:
+  %%className%% (%%constructorParams%%);
+  ~%%className%%();
+
+  //[UserMethods]     -- You can add your own custom methods in this section.
+  //[/UserMethods]
+
+  %%publicMemberDeclarations%%
+
+ private:
+  //[UserVariables]   -- You can add your own custom variables in this section.
+  //[/UserVariables]
+
+  // Member variables added by the jucer.
+  %%privateMemberDeclarations%%
+
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(%%className%%)
+};
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
+#endif   // %%headerGuard%%
